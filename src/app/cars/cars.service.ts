be8fe05a2b6dbcd6930b0d6505560ee82cb4a6a5
Rafał Janeczek <rafaljaneczek@o2.pl>
2022-11-01 14:16:05 +0100

    change REST API url

--- src/app/cars/cars.service.ts
+++ src/app/cars/cars.service.ts
@@ -8,41 +8,41 @@ import {HttpClient} from "@angular/common/http";
 })
 export class CarsService {
 
-  private apiUrl = "http://localhost:3000/api/cars";
+  private apiUrl = "http://localhost:8083/car";
 
   constructor(private http: HttpClient) {
   }
 
   getCars(): Observable<Car[]> {
-    return this.http.get<Car[]>(this.apiUrl)
+    return this.http.get<Car[]>(this.apiUrl + '/get-all')
       .pipe(map((response: Car[]) => {
         return response as Car[];
       }))
   }
 
   getCar(id: number): Observable<Car> {
-    return this.http.get<Car>(this.apiUrl + `/${id}`)
+    return this.http.get<Car>(this.apiUrl + `/get/${id}`)
       .pipe(map((response: Car) => {
         return response as Car;
       }))
   }
 
   addCar(data: any): Observable<Car> {
-    return this.http.post<Car>(this.apiUrl, data)
+    return this.http.post<Car>(this.apiUrl + '/add', data)
       .pipe(map((response: Car) => {
         return response as Car;
       }))
   }
 
   updateCar(id: number, data: Car): Observable<Car> {
-    return this.http.put<Car>(this.apiUrl + `/${id}`, data)
+    return this.http.put<Car>(this.apiUrl + `/update/${id}`, data)
       .pipe(map((response: Car) => {
         return response as Car;
       }))
   }
 
   removeCar(id: number): Observable<Car> {
-    return this.http.delete<Car>(this.apiUrl + `/${id}`)
+    return this.http.delete<Car>(this.apiUrl + `/remove/${id}`)
       .pipe(map((response: Car) => {
         return response as Car;
       }))
